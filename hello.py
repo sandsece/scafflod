84e01ba38853a83b35948835ada744c666c0f5d9
EC2 Default User <ec2-user@ip-172-31-16-47.us-west-2.compute.internal>
2022-06-27 11:41:19 +0000

    add new subtract method

--- hello.py
+++ hello.py
@@ -2,5 +2,9 @@ def add(x, y):
     return x + y
 
 
+def subtract(x, y):
+    return x - y
+
+
 result = add(1, 2)
 print(f"this is the sum for 1,2, {result}")
--- test_hello.py
+++ test_hello.py
@@ -1,6 +1,11 @@
 from hello import add
+from hello import subtract
 
 
 def test_add():
     assert add(2, 2) == 4
     assert add(2, 3) != 4
+
+
+def test_subtract():
+    assert subtract(5, 4) == 1
